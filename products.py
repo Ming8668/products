dc16c2dcafc4dbef28bdc8f67fdd4b3c2798d4e7
Ming8668 <mike86.chan@gmail.com>
2024-04-28 13:54:07 +0800

    add header

--- products.py
+++ products.py
@@ -7,6 +7,7 @@ while True:
 	if name == 'q':
 		break
 	price = input('請輸入商品價格:')
+	price = int(price)
 
 	products.append([name, price])
 
@@ -14,3 +15,10 @@ print(products)
 
 for p in products:
 	print(p[0], '的價格是', p[1])
+
+
+# 寫入檔案
+with open('products.csv', 'w', encoding='utf-8') as f: #沒有會創建檔案, 有的話會覆蓋
+	f.write('商品,價格\n') # 需指定編碼(encoding)才不會便亂碼 # 用excel要用utf-8打開
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n') # 字串字串才能合併
