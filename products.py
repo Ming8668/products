cd0dfe543f0378df24af240a28bb56859487b177
Ming8668 <mike86.chan@gmail.com>
2024-04-28 14:23:48 +0800

    add read file

--- products.py
+++ products.py
@@ -2,6 +2,29 @@
 
 products = []
 
+# 讀取檔案
+with open('products.csv', 'r', encoding='utf-8') as f:
+	for line in f:
+		if '商品,價格' in line: 
+			continue # 繼續 #不執行迴圈跳到下一個迴圈
+		name, price = line.strip().split(',') # 先把\n去掉，再遇到逗點切一刀
+		products.append([name, price])
+# print(products)
+
+
+# 印出歷史紀錄
+print('目前記帳紀錄:')
+for p in products:
+	print(p[0], '的價格是', p[1])
+print('-' * 40)
+
+
+#提醒
+print('提醒: 輸入結束請輸入 q')
+print('-' * 40)
+
+
+# 讓使用者輸入
 while True:
 	name = input('請輸入商品名稱:')
 	if name == 'q':
@@ -10,9 +33,10 @@ while True:
 	price = int(price)
 
 	products.append([name, price])
-
 print(products)
 
+
+# 印出所有購買紀錄
 for p in products:
 	print(p[0], '的價格是', p[1])
 
